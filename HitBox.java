faa7ae2866629afacbbfbc4104b928b067c60e37
LeoCai12777 <leo.cai12777@gmail.com>
2017-06-19 08:34:54 -0400

    Create HitBox.java

---
+++ HitBox.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HitBox {
+    private int x, y;
+    
+    public HitBox(int xPos, int yPos)
+    {
+        x = xPos;
+        y = yPos;
+    }// hitbox will extend: for Aliens, down 30 right 30, for Ship: down 15, right 10
+}
