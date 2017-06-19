c5813ec3723fea474e45e5d59b0d38f3fd260ff2
LeoCai12777 <leo.cai12777@gmail.com>
2017-06-19 08:34:57 -0400

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -1,26 +1,18 @@
 import java.util.*;
 
 public class Bullet {
-    public int xPos, yPos;
+    public int xPos, yPos, vy;
     private Timer timer;
-    private boolean ship = false;
+    private boolean ship = false, hit;
     public Bullet (int x, int y, boolean s) {
         xPos = x;
         yPos = y;
+        vy = 15; //speed of bullet in y direction; assuming bullet only moves up and down, vx not needed
         if (s)
-            ship = true;
+            vy = -15; // if player, bullet moves up
     }
 
-    public void shoot () {
-        timer = new Timer ();
-        timer.schedule(new ShootTask(),0, 30);
-    }
-    class ShootTask extends TimerTask {
-        public void run () {
-            if (ship)
-                yPos += 1;
-            else
-                yPos -= 1;
-        }
+    public void checkHit (HitBox box) {
+        
     }
 }
