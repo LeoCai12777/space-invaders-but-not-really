9cac9f366ff2c3b18c62511d860fdb15cd3f1dd7
LeoCai12777 <leo.cai12777@gmail.com>
2017-06-19 22:05:40 -0400

    Update Dialogue.java

--- Dialogue.java
+++ Dialogue.java
@@ -7,47 +7,47 @@ public class Dialogue // pop up text box, offers options like Jay's problem set 
     {
     }
 
-    public int talkAlien() //returns a difficulty level from 1-3, 1->easy, 3->hard
+    public int talkAlien(TextBox textBox) //returns a difficulty level from 1-3, 1->easy, 3->hard
     {
         char choice;
         int difficulty=2;
-        System.out.println("General Zorp-Gorp: Greetings human. We have decided this planet is ours for the taking.");
+        textBox.displayLine("General Zorp-Gorp: Greetings human. We have decided this planet is ours for the taking.");
         sc.nextLine();
-        System.out.println("1. Isn't there a peaceful way to resolve this?");
-        System.out.println("2. We'll see about that.");
-        System.out.println("3. Alien scum! I'll blast you back to Andromeda!");
+        textBox.displayLine("1. Isn't there a peaceful way to resolve this?");
+        textBox.displayLine("2. We'll see about that.");
+        textBox.displayLine("3. Alien scum! I'll blast you back to Andromeda!");
         do{
             choice = sc.nextLine().charAt(0); //takes first character of next sentence
         }while((int)choice<49||(int)choice>51); //forces choice to be between 1 and 3
         if(choice == '1')
         {
-            System.out.println("General Zorp-Gorp: Well, we planned to invade, but perhaps we can make a deal.");
+            textBox.displayLine("General Zorp-Gorp: Well, we planned to invade, but perhaps we can make a deal.");
             sc.nextLine();
-            System.out.println("1. I'll have to ask my superiors. Could you wait a few hours?")
-            System.out.println("2. Barter? You must be joking. We expect you to leave and to leave now.");
-            System.out.println("3. Great! Let's talk. What would you like?");
+            textBox.displayLine("1. I'll have to ask my superiors. Could you wait a few hours?")
+            textBox.displayLine("2. Barter? You must be joking. We expect you to leave and to leave now.");
+            textBox.displayLine("3. Great! Let's talk. What would you like?");
             do{
             choice = sc.nextLine().charAt(0); //takes first character of next sentence
             }while((int)choice<49||(int)choice>51); //forces choice to be between 1 and 3
             if(choice == '1')
-                System.out.println("General Zorp-Gorp: I'm afraid that will take too long.");
+                textBox.displayLine("General Zorp-Gorp: I'm afraid that will take too long.");
             else if(choice == '2')
             {
-                System.out.println("General Zorp-Gorp: You do not respect us enough to trade?");
+                textBox.displayLine("General Zorp-Gorp: You do not respect us enough to trade?");
                 difficulty = 3;
             }
             else if(choice == '3')
             {
-                System.out.println("General Zorp-Gorp: We have come to harvest the human species. \nOur race thrives on cuisine and human is said to taste great.");
+                textBox.displayLine("General Zorp-Gorp: We have come to harvest the human species. \nOur race thrives on cuisine and human is said to taste great.");
                 sc.nextLine();
-                System.out.println("1. Want to try hunting? I think it would be more fun if you had less ships.");
-                System.out.println("2. That's disgusting. How could you eat another sentient being?");
+                textBox.displayLine("1. Want to try hunting? I think it would be more fun if you had less ships.");
+                textBox.displayLine("2. That's disgusting. How could you eat another sentient being?");
                 do{
                 choice = sc.nextLine().charAt(0); //takes first character of next sentence
                 }while((int)choice<49||(int)choice>51); //forces choice to be between 1 and 3
                 if(choice == '1')
                 {
-                    System.out.println("General Zorp-Gorp: That sounds like great fun!");
+                    textBox.displayLine("General Zorp-Gorp: That sounds like great fun!");
                     difficulty = 1;
                 }
                 else if(choice == '2')
@@ -88,16 +88,16 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
  
-public class TextDemo extends JPanel implements ActionListener {
-    protected JTextField textField;
+public class TextBox extends JPanel implements ActionListener {
+//     protected JTextField textField;
     protected JTextArea textArea;
-    private final static String newline = "\n";
+//     private final static String newline = "\n";
  
-    public TextDemo() {
+    public TextBox() {
         super(new GridBagLayout());
  
-        textField = new JTextField(20);
-        textField.addActionListener(this);
+//         textField = new JTextField(20);
+//         textField.addActionListener(this);
  
         textArea = new JTextArea(5, 20);
         textArea.setEditable(false);
@@ -108,23 +108,26 @@ public class TextDemo extends JPanel implements ActionListener {
         c.gridwidth = GridBagConstraints.REMAINDER;
  
         c.fill = GridBagConstraints.HORIZONTAL;
-        add(textField, c);
+//         add(textField, c);
  
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1.0;
         c.weighty = 1.0;
         add(scrollPane, c);
     }
- 
-    public void actionPerformed(ActionEvent evt) {
-        String text = textField.getText();
-        textArea.append(text + newline);
-        textField.selectAll();
- 
-        //Make sure the new text is visible, even if there
-        //was a selection in the text area.
-        textArea.setCaretPosition(textArea.getDocument().getLength());
+    public void displayLine(String msg)
+    {
+        textArea.append(msg+"\n");
     }
+//     public void actionPerformed(ActionEvent evt) {
+//         String text = textField.getText();
+//         textArea.append(text + newline);
+//         textField.selectAll();
+ 
+//         //Make sure the new text is visible, even if there
+//         //was a selection in the text area.
+//         textArea.setCaretPosition(textArea.getDocument().getLength());
+//     }
  
     /**
      * Create the GUI and show it.  For thread safety,
@@ -133,11 +136,11 @@ public class TextDemo extends JPanel implements ActionListener {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("TextDemo");
+        JFrame frame = new JFrame("TextBox");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
         //Add contents to the window.
-        frame.add(new TextDemo());
+        frame.add(new TextBox());
  
         //Display the window.
         frame.pack();
